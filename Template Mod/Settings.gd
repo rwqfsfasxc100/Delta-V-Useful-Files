650d0c0b98572ced3da875e80c71f488d5834ea6
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-04-13 11:28:51 -0700

    Update Settings.gd

--- Template Mod/Settings.gd
+++ Template Mod/Settings.gd
@@ -1,14 +1,10 @@
 extends "res://Settings.gd"
 
-# Mod version
-const EXAMPLE_VERSION = "1.0.0"
-
-
 # You may want to change many of the variable names to provide a unique identifier
 # Make sure anything read by the ModMain is consistent with this file or they will not work
 # These are default config values
 # Any value not set in the config file will generate the missing values exactly as these are
-var ExampleConfig = {
+var ExampleMod = {
 	"section":{
 		"setting":true,
 		"string_option":"setting_value"
@@ -24,36 +20,44 @@ var ExampleConfig = {
 }
 
 # The config file name. Make sure you set something unique
-var ExamplePath = "user://Examplesettings.cfg"
-var ExampleFile = ConfigFile.new()
+# Config is set to the cfg folder to make it easy to find
+var ConfigPath = "user://cfg/ExampleMod.cfg"
+var ConfigFile = ConfigFile.new()
 
 func _ready():
-	loadExampleFromFile()
-	saveExampleToFile()
+	var dir = Directory.new()
+	dir.make_dir("user://cfg")
+	loadExampleModFromFile()
+	saveExampleModToFile()
 
 
-func saveExampleToFile():
-	for section in ExampleConfig:
-		for key in ExampleConfig[section]:
-			ExampleFile.set_value(section, key, ExampleConfig[section][key])
-	ExampleFile.save(ExamplePath)
+func saveExampleModToFile():
+	for section in ExampleMod:
+		for key in ExampleMod[section]:
+			ConfigFile.set_value(section, key, ExampleMod[section][key])
+	ConfigFile.save(ConfigPath)
 
 
-func loadExampleFromFile():
-	var error = ExampleFile.load(ExamplePath)
+func loadExampleModFromFile():
+	var error = ConfigFile.load(ConfigPath)
 	if error != OK:
 		Debug.l("Example Mod: Error loading settings %s" % error)
 		return 
-	for section in ExampleConfig:
-		for key in ExampleConfig[section]:
-			ExampleConfig[section][key] = ExampleFile.get_value(section, key, ExampleConfig[section][key])
-	loadExampleKeymapFromConfig()
+	for section in ExampleMod:
+		for key in ExampleMod[section]:
+			ExampleMod[section][key] = ConfigFile.get_value(section, key, ExampleMod[section][key])
+	loadKeymapFromConfig()
 
 # Keybind setting handlers
-func loadExampleKeymapFromConfig():
-	for action_name in ExampleConfig.input:
-		InputMap.add_action(action_name)
-		for key in ExampleConfig.input[action_name]:
+func loadKeymapFromConfig():
+	for action_name in ExampleMod.input:
+		var addAction = true
+		for m in actions:
+			if m == action_name:
+				addAction = false
+		if addAction:
+			InputMap.add_action(action_name)
+		for key in ExampleMod.input[action_name]:
 			var event = InputEventKey.new()
 			event.scancode = OS.find_scancode_from_string(key)
 			InputMap.action_add_event(action_name, event)
